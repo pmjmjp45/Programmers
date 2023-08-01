492f509b6870a6fab3bc94d421d7c3d02dfcbaf5
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2023-08-01 14:03:49 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 2.92 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,57 +1,38 @@
 import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        //terms 정보 hashmap에 넣기
-        HashMap<String, Integer> termsMap = new HashMap<>();
-        
-        for (String str : terms) {
-            termsMap.put(str.substring(0, 1), Integer.parseInt(str.substring(2))); 
-        }
-        
-        //비교하기
-        ArrayList<Integer> answer0 = new ArrayList<>();
-        
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
             String start = privacies[i].substring(0, 10);
-            String rank = privacies[i].substring(11);
+            String level = privacies[i].substring(11);
             
-            //유효기간
-            int limit = termsMap.get(rank) * 28;
+            //규약 깨지는 날
+            int endMonth = 0; // 유효기간
+            for (String t : terms) {
+                if (t.startsWith(level)) {
+                    endMonth = Integer.parseInt(t.substring(2));
+                    break;
+                }
+            }
+            int endDay = (endMonth * 28) + calDays(start);
             
-            //today - start
-            int daysGoneBy = calDays(today, start);
+            //오늘 
+            int todayCal = calDays(today);
             
-            if (daysGoneBy >= limit) {
-                answer0.add(i + 1);
+            if (endDay <= todayCal) {
+                list.add(i + 1);
             }
         }
-        
-        int[] answer = answer0.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
-    
-    //날짜 계산 메서드
-    public static int calDays(String today, String start) {
-        int tYear = Integer.parseInt(today.substring(0, 4));
-        int tMonth = Integer.parseInt(today.substring(5, 7));
-        int tDay = Integer.parseInt(today.substring(8, 10));
-        int sYear = Integer.parseInt(start.substring(0, 4));
-        int sMonth = Integer.parseInt(start.substring(5, 7));
-        int sDay = Integer.parseInt(start.substring(8, 10));
+    // 2000.01.01부터 몇일이 지났나 계산
+    public int calDays(String day) {
+        int year = Integer.parseInt(day.substring(0, 4));
+        int month = Integer.parseInt(day.substring(5, 7));
+        int date = Integer.parseInt(day.substring(8, 10));
         
-        //2000.01.01부터 today의 날짜는?
-        int tTotal = 0;
-        tTotal = (tYear - 2000) * 12 * 28;
-        tTotal += tMonth * 28;
-        tTotal += tDay;
-        
-        //2000.01.01부터 start의 날짜는?
-        int sTotal = 0;
-        sTotal = (sYear - 2000) * 12 * 28;
-        sTotal += sMonth * 28;
-        sTotal += sDay;
-        
-        return tTotal - sTotal;
+        int days = (year - 2000) * 12 * 28 + (month - 1) * 28 + date - 1;
+        return days;
     }
-    
 }
