25a39d351a206bf4f7ada6cff6757193b7d64acf
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2023-08-01 10:02:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.28 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,31 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int[] array) {
-        //해쉬맵에 값 넣기
+        //최대 빈도 찾기
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : array) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for (int x : array) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
         
-        //최고빈도 구하기
-        int max = 0;
-        for (int key : map.keySet()) {
-            max = Math.max(map.get(key), max);
-        }
-        
-        //최빈값 구하기(최빈값 개수 구하기)
-        ArrayList<Integer> answerList = new ArrayList<>();
-        
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                answerList.add(key);
+        int max = 0; // 최대 빈도
+        for (int x : map.keySet()) {
+            if (map.get(x) > max) {
+                max = map.get(x);
             }
         }
+        //최대 빈도의 값 개수
+        ArrayList<Integer> answer0 = new ArrayList<>();
         
-        if (answerList.size() == 1) {
-            return answerList.get(0); 
-        } else {
+        for (int x : map.keySet()) {
+            if (map.get(x) == max) {
+                answer0.add(x);
+            }
+        }
+        if (answer0.size() > 1) {
             return -1;
+        } else {
+           return answer0.get(0); 
         }
     }
 }
