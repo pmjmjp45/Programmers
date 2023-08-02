06650bd15dc6eda37e3a7c0ee82e321a2135fc01
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2023-08-02 17:36:47 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,6 +1,12 @@
 -- 코드를 입력하세요
+# SELECT DISTINCT a.CART_ID
+# FROM CART_PRODUCTS AS a
+# JOIN CART_PRODUCTS AS b ON a.CART_ID = b.CART_ID && b.ID > a.ID
+# WHERE (a.NAME = 'Milk' && b.NAME = 'Yogurt') || (a.NAME = 'Yogurt' && b.NAME = 'Milk')
+# ORDER BY a.CART_ID
+
 SELECT DISTINCT a.CART_ID
 FROM CART_PRODUCTS AS a
-JOIN CART_PRODUCTS AS b ON a.CART_ID = b.CART_ID && b.ID > a.ID
-WHERE (a.NAME = 'Milk' && b.NAME = 'Yogurt') || (a.NAME = 'Yogurt' && b.NAME = 'Milk')
-ORDER BY a.CART_ID
+JOIN CART_PRODUCTS AS b ON a.CART_ID = b.CART_ID
+WHERE (a.NAME = 'Milk' && b.NAME = 'Yogurt')
+ORDER BY a.CART_ID 
