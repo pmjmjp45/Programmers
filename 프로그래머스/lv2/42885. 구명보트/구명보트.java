6987f7d563f97c6a99bf478e7e284b84efc4b35f
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2023-08-04 18:57:44 +0900

    [level 2] Title: 구명보트, Time: 9.04 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        //정렬한 뒤 가장 가벼운 사람과 가장 무거운 사람의 합과 limit 비교
+        // 합 <= limit : 한꺼번에 옮김
+        // 합 > limit : 무거운 사람만 옮김
+        Arrays.sort(people);
+        
+        int right = people.length - 1; //가장 무거운 사람의 인덱스
+        int left = 0; // 가장 가벼운 사람의 인덱스
+        int answer = 0;
+        
+        while(left + 1 < right) {
+            if (people[left] + people[right] <= limit) {
+                answer++;
+                left++;
+                right--;
+            } else {
+                answer++;
+                right--;
+            }
+        }
+        
+        //인접했을 때는 따로 빼서 해줘야 함
+        if (people[left] + people[right] <= limit) {
+            answer++;
+        } else {
+            answer += 2;
+        }
+        return answer;
+    }
+}
