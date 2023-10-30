63e4d2a94c75e6b839e751ede305df79cd218ab0
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2023-10-30 16:12:41 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    if (n <= 7) {
+        answer = 1;
+    } else if (n % 7 == 0) {
+        answer = parseInt(n / 7);
+    } else {
+        answer = parseInt(n / 7) + 1;
+    }
+    return answer;
+}
