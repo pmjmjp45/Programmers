df20757cd9d473f7c379abdd2c326d29c68a1731
pmjmjp45 <127267641+pmjmjp45@users.noreply.github.com>
2025-02-06 14:36:47 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        int idx = 0;
+        while(answer <= n) {
+            answer += numbers[idx];
+            idx++;
+        }
+        return answer;
+    }
+}
